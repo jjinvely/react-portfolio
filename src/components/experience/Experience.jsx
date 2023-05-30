7376140b2c934b59666458d68b0104925dbae547
yeojin Kim <jjiny2022@kakao.com>
2023-05-30 20:34:48 +0900

    .
    .

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,8 +5,7 @@ import {BsPatchCheckFill} from 'react-icons/bs'
 function Experience() {
   return (
    <section id="experience">
-    <h5>What Skills I Have</h5>
-    <h2>My Experience</h2>
+    <h2>My Skillss</h2>
 
     <div className="container experience__container">
       <div className="experience__frontend">
