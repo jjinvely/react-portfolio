5fb7a4833180da9d958041723ed0766f2180dfa9
yeojin Kim <jjiny2022@kakao.com>
2023-05-30 20:34:41 +0900

    .
    .

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -39,7 +39,6 @@ const data = [
 function Portfolio() {
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
        {
